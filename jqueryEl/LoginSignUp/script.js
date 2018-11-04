7d8d66a250f5f17c6fd79080e5c06058f2b232e5
Amit <amit0805kumar@gmail.com>
2018-11-05 00:23:27 +0530

    Form toggle working

--- jqueryEl/LoginSignUp/script.js
+++ jqueryEl/LoginSignUp/script.js
@@ -9,13 +9,33 @@ $(function () {
     var sliderWidth = $('.slider').css('width').replace('px', '');
     slider.css('transform', 'translateX(-' + sliderWidth / 2 + 'px)');
 
+    console.log(sliderWidth);
+
     slider.css('margin-left', sliderMargin + "rem");
 
+    $('.signUpForm').hide();
+    
+    login.fadeOut();
     login.stop().click(function () {
+        
+        $('.loginForm').fadeIn(400);
+        $('.signUpForm').fadeOut(400);
+        slider.css("transition", "all .3s cubic-bezier(.87,.32,.79,.6)");
         slider.css('margin-left', sliderMargin + "rem");
+        
+        login.fadeOut();
+        signup.fadeIn();
     });
     signup.stop().click(function () {
-        slider.css('margin-left', sliderMargin * 3 + "rem");
+        $('.loginForm').fadeOut(400);
+        $('.signUpForm').fadeIn(400);
+        slider.css("transition", "all .5s cubic-bezier(.87,.32,.79,.6)");
+        slider.css('margin-left', sliderMargin * 3 + "rem");        
+        signup.fadeOut()
+        login.fadeIn();
+        
     });
 
+    
+    
 });
