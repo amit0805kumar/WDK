1eea9489df50ea3f63cd6420475166477a1c84cb
Amit <amit0805kumar@gmail.com>
2018-11-08 23:47:12 +0530

    Some special jquery framework applied
    hover3d.js
    parallex.js
    wow.js

--- jqueryEl/Parallax/script.js
+++ jqueryEl/Parallax/script.js
@@ -1,4 +1,36 @@
 $(function () {
+    
+    
+    var rellax = new Rellax('.rellax', {
+    speed: -4,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
+/*        var rellax2 = new Rellax('.rellax2', {
+    speed: -1,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });*/
+/*        var rellax3 = new Rellax('.rellax3', {
+    speed: 0,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
+    */
+    
+    
+    
+       new WOW().init();
+    
     $(window).scroll(function () {
 
 
@@ -6,5 +38,17 @@ $(function () {
         $("#logo").css("transform", "translate(0px,-" + scrollVal / 2 + "%)");
         $("#btn").css("transform", "translate(-50%," + scrollVal / 3 + "%)");
 
+
+    });
+
+    $(".project").hover3d({
+        selector: ".project__card",
+        shine: true,
+        sensitivity: 20,
+
+    });
+    $("#main").hover3d({
+        selector: "#logoImg",
+        sensitivity: 20,
     });
 });
