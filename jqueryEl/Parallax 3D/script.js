894b1c5dc423814ba97e2b286f09f86310d0561f
Amit <amit0805kumar@gmail.com>
2018-11-12 16:14:23 +0530

    Image modified

--- jqueryEl/Parallax 3D/script.js
+++ jqueryEl/Parallax 3D/script.js
@@ -1,87 +1,46 @@
 $(function () {
-    
-    
-//    var rellax = new Rellax('.rellax', {
-//    speed: -4,
-//    center: false,
-//    wrapper: null,
-//    round: true,
-//    vertical: true,
-//    horizontal: false
-//  });
-/*        var rellax2 = new Rellax('.rellax2', {
-    speed: -1,
-    center: false,
-    wrapper: null,
-    round: true,
-    vertical: true,
-    horizontal: false
-  });*/
-        var rellax1 = new Rellax('.rellax1', {
-            speed: -7,
-            center: false,
-            wrapper: null,
-            round: true,
-            vertical: true,
-            horizontal: false
-        });
-    
-        var rellax2 = new Rellax('.rellax2', {
-            speed: -5,
-            center: false,
-            wrapper: null,
-            round: true,
-            vertical: true,
-            horizontal: false
-        });
-        var rellax4 = new Rellax('.rellax4', {
-            speed: -3,
-            center: false,
-            wrapper: null,
-            round: true,
-            vertical: true,
-            horizontal: false
-        });
-      var rellax3 = new Rellax('.rellax3', {
-            speed: 0,
-            center: false,
-            wrapper: null,
-            round: true,
-            vertical: true,
-            horizontal: false
-        });
-        var rellax5 = new Rellax('.rellax5', {
-            speed: 2,
-            center: false,
-            wrapper: null,
-            round: true,
-            vertical: true,
-            horizontal: false
-        });
 
-    
-    
-    
-    
-  /*  
-    $(window).scroll(function () {
+    var rellax1 = new Rellax('.rellax1', {
+        speed: -7,
+        center: false,
+        wrapper: null,
+        round: true,
+        vertical: true,
+        horizontal: false
+    });
+
+    var rellax2 = new Rellax('.rellax2', {
+        speed: -5,
+        center: false,
+        wrapper: null,
+        round: true,
+        vertical: true,
+        horizontal: false
+    });
+    var rellax4 = new Rellax('.rellax4', {
+        speed: -3,
+        center: false,
+        wrapper: null,
+        round: true,
+        vertical: true,
+        horizontal: false
+    });
+    var rellax3 = new Rellax('.rellax3', {
+        speed: 0,
+        center: false,
+        wrapper: null,
+        round: true,
+        vertical: true,
+        horizontal: false
+    });
+    var rellax5 = new Rellax('.rellax5', {
+        speed: 2,
+        center: false,
+        wrapper: null,
+        round: true,
+        vertical: true,
+        horizontal: false
+    });
 
 
-        var scrollVal = $(this).scrollTop();
-        $("#logo").css("transform", "translate(0px,-" + scrollVal / 2 + "%)");
-        $("#btn").css("transform", "translate(-50%," + scrollVal / 3 + "%)");
-
-
-    });*/
-
- /*   $(".header").hover3d({
-        selector: ".pngs",
-        shine: true,
-        sensitivity: 50,
-
-    });*/
-//    $("#main").hover3d({
-//        selector: "#logoImg",
-//        sensitivity: 20,
-//    });
 });
